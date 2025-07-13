ff69b9edd1d0c6bd9a14fac030f60764d7a2e646
Tahsin Ahmed <tahsinahmed2998@gmail.com>
2025-07-13 21:57:36 +0600

    Contact version 1

---
+++ Contact.php
@@ -0,0 +1,64 @@
+<?php
+
+$contacts = [];
+
+function addContact(array &$contacts, string $name, string $email, int $phone): void
+{
+    $contacts[] = 
+    [
+        'name' => $name,
+        'email' => $email,
+        'phone' => $phone
+    ];
+};
+
+function getContacts(array $contacts): array
+{
+    if(empty($contacts)) {
+        return ['message' => 'No contacts found.'];
+    }else {
+        foreach($contacts as $contact){
+            echo "
+                Name: {$contacts ['name']};
+                Email: {$contacts ['email']};
+                Phone: {$contacts ['phone']} \n;
+            ";
+        }
+    }
+
+    return $contacts;
+};
+
+
+while(true) {
+    echo "Contact Management App";
+    echo "
+    Press 1 to add contact.
+    Press 2 to view contact.
+    Press 3 to exit the App.
+    ";
+
+    $choose = (int)(readline("Choose an option: "));
+
+    if($choose === 1) {
+        $name = readline("Enter contact name: ");
+        $email = readline("Enter contact email: ");
+        $phone = (int)readline("Enter contact phone number: ");
+        
+        addContact($contacts, $name, $email, $phone);
+        echo "Contact added successfully! \n";
+    } elseif($choose === 2) {
+        $result = getContacts($contacts);
+        if(isset($result['message'])) {
+            echo $result['message'] . "\n";
+        }
+    } elseif($choose === 3) {
+        echo "Exiting the app. Goodbye! \n";
+        break;
+    } else {
+        echo "Invalid option. Please try again. \n";
+    }
+    echo "\n";
+}
+
+?>
